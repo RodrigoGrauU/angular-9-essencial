47c35a40a909bbb4d7d4a6e6db5e332898fc3d19
RodrigoGrauU <rodrigoassis1997@gmail.com>
2022-11-14 23:52:30 -0300

    Desafio Componente Excluir Produto - Resposta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
 import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
@@ -22,6 +23,10 @@ const routes: Routes = [
   {
     path: "products/update/:id",
     component: ProductUpdateComponent
+  },
+  {
+    path: "products/delete/:id",
+    component: ProductDeleteComponent
   }
 ];
 
---
+++ src/app/components/product/product-delete/product-delete.component.ts
@@ -0,0 +1,41 @@
+import { ProductService } from './../product.service';
+import { Product } from './../product.model';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-product-delete',
+  templateUrl: './product-delete.component.html',
+  styleUrls: ['./product-delete.component.css']
+})
+export class ProductDeleteComponent implements OnInit {
+
+  product!: Product;
+
+  constructor(
+    private productService: ProductService, 
+    private router: Router,
+    private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id') || ''
+    this.productService.readById(id).subscribe(product => {
+      this.product = product
+    })
+  }
+
+  deleteProduct(): void {
+    if(this.product.id == undefined) {
+      this.product.id = 0
+    }
+    
+    this.productService.delete(this.product.id).subscribe(() => {
+      this.productService.showMessage('Produto excluído com sucesso')
+      this.router.navigate(['products'])
+    })
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
+  }
+}
